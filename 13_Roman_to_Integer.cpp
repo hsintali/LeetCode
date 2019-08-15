dbd26bcd091673a5c6e3edc9f6910d61a72e6079
Hsin-Ta Li <47123885+hsintali@users.noreply.github.com>
2019-08-15 14:25:09 -0700

    Create 13_Roman_to_Integer.cpp
    https://leetcode.com/problems/roman-to-integer/

---
+++ 13_Roman_to_Integer.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        int *number = new int[s.size()];
+        int n = 0;
+        for(int i = 0; i < s.size(); ++i)
+        {
+            switch(s[i])
+            {
+                case 'I':
+                    number[i] = 1;
+                    break;
+                case 'V':
+                    number[i] = 5;
+                    break;
+                case 'X':
+                    number[i] = 10;
+                    break;
+                case 'L':
+                    number[i] = 50;
+                    break;
+                case 'C':
+                    number[i] = 100;
+                    break;
+                case 'D':
+                    number[i] = 500;
+                    break;
+                case 'M':
+                    number[i] = 1000;
+                    break;
+            }
+            n += number[i];
+            
+            if(i == 0)
+                continue;
+            
+            if(number[i] > number[i-1])
+                n -= 2*number[i-1];
+        }
+        
+        delete []number;
+        
+        return n;
+    }
+};
