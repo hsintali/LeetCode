5359db9219582ac6c9ce3ef366e22292f37d5f15
Hsin-Ta Li <47123885+hsintali@users.noreply.github.com>
2019-08-16 16:10:36 -0700

    Create 125. valid-palindrome.cpp

---
+++ 125. valid-palindrome.cpp
@@ -0,0 +1,28 @@
+/*
+https://leetcode.com/problems/valid-palindrome/
+*/
+
+
+class Solution {
+public:
+    bool isPalindrome(string s)
+    {
+        if(s.size() == 0) return true;
+        
+        int left = 0;
+        int right = s.size() - 1;
+        
+        while(left < right)
+        {
+            while(!isalnum(s[left]) && left < right) ++left;
+            while(!isalnum(s[right]) && left < right) --right;
+
+            if(toupper(s[left]) != toupper(s[right])) return false;
+            
+            ++left;
+            --right;
+        }
+        
+        return true;
+    }
+};
