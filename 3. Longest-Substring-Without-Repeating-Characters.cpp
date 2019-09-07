8e1aa1bee55ff519aabcaa98ffc161407ab94c2e
Hsin-Ta Li <47123885+hsintali@users.noreply.github.com>
2019-09-06 17:05:16 -0700

    Update 3. Longest-Substring-Without-Repeating-Characters.cpp

--- 3. Longest-Substring-Without-Repeating-Characters.cpp
+++ 3. Longest-Substring-Without-Repeating-Characters.cpp
@@ -5,7 +5,7 @@ Intuition:
 Check all the substring one by one to see if it has no duplicate character by hashset.
 
 Time complexity: O(n^2)
-Space complexity: O(k), where kk is the size of the Set k <= n
+Space complexity: O(k), where k is the size of the set, k <= n
 */
 
 class Solution {
@@ -45,7 +45,7 @@ public:
 /* Approach 2: Sliding Window
 
 Time complexity: O(2n)
-Space complexity: O(k), where kk is the size of the Set k <= n
+Space complexity: O(k), where k is the size of the set, k <= n
 */
 
 class Solution {
@@ -69,6 +69,35 @@ public:
             }          
         }
              
+        return length;
+    }
+};
+
+/* Approach 3: Sliding Window Optimized
+Intuition:
+skip all the elements in the range [i, j'] and let i to be j' + 1 directly.
+
+Time complexity: O(n)
+Space complexity: O(k), where k is the size of the set k <= n
+*/
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s)
+    {   
+        int length = 0;
+        unordered_map<char, int> substring;
+        
+        for(int left = 0, right = 0; right < s.size(); ++right)
+        {
+            if(substring.find(s[right]) != substring.end())
+            {
+                left = max(left, substring[s[right]] + 1);
+            }            
+            length = max(length, right - left + 1);
+            substring[s[right]] = right;
+        }
+        
         return length;
     }
 };
