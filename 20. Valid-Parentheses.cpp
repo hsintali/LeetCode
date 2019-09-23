e8f045768fd78c0dfadbe8b9181453c57c7235c1
Hsin-Ta Li <47123885+hsintali@users.noreply.github.com>
2019-09-23 16:52:30 -0700

    Update 20. Valid-Parentheses.cpp

--- 20. Valid-Parentheses.cpp
+++ 20. Valid-Parentheses.cpp
@@ -3,6 +3,37 @@
 /* Approach 1: stack
 Intuition:
 
-Time complexity: O()
-Space complexity: O()
+Time complexity: O(n)
+Space complexity: O(n)
 */
+
+class Solution {
+public:
+    bool isValid(string s) {
+        unordered_map<char, char> mapping = {{')', '('}, {']', '['}, {'}', '{'}};
+        
+        stack<char> stack_;
+        for(int i = 0; i < s.size(); ++i)
+        {
+            if(mapping.count(s[i]) == 1) //encounter closing bracket
+            {
+                char c = stack_.empty()? '#' : stack_.top();
+                if(c == mapping[s[i]])
+                {
+                    stack_.pop();
+                }
+                else
+                {
+                    return false;
+                }
+                    
+            }
+            else //encounter opening bracket
+            {
+                stack_.push(s[i]);
+            }
+        }
+        
+        return stack_.empty() ? true : false;
+    }
+};
