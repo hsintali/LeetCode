60220fc3719776802d5411ee59640795c71d2463
Hsin-Ta Li <47123885+hsintali@users.noreply.github.com>
2019-09-23 15:35:39 -0700

    Update 14. Longest-Common-Prefix.cpp

--- 14. Longest-Common-Prefix.cpp
+++ 14. Longest-Common-Prefix.cpp
@@ -28,3 +28,57 @@ public:
         return prefix;
     }
 };
+
+/* Approach 2: Divide and conquer
+Intuition:
+
+Time complexity: O(m*n). In the worst case there will be n equal strings with length m.
+                 = T(n/2) + O(M)
+Space complexity: O(mlogn)
+*/
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs)
+    {
+        if(strs.empty())
+            return "";
+        
+        return LCP(strs, 0, strs.size() - 1);
+    }
+    
+    string LCP(vector<string> &strs, int l, int r)
+    {
+        if(l == r)
+        {
+            return strs[r];
+        }
+        
+        int mid = l + (r-l)/2;
+        string leftString = LCP(strs, l, mid);
+        string rightString = LCP(strs, mid + 1, r);
+
+        return commonPrefix(leftString, rightString);
+    }
+    
+    string commonPrefix(string &leftString, string &rightString)
+    {
+        int length = min(leftString.size(), rightString.size());
+        for(int i = 0; i < length; ++i)
+        {
+            if(leftString[i] != rightString[i])
+            {
+                return rightString.substr(0, i);
+            }
+        }
+        
+        return rightString.substr(0, length);
+    }
+};
+
+
+
+
+
+
+
