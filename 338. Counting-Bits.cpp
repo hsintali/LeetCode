115da82412e576823a390198ff3325ea41c2db6a
Hsin-Ta Li <47123885+hsintali@users.noreply.github.com>
2019-10-19 16:22:36 -0700

    Update 338. Counting-Bits.cpp

--- 338. Counting-Bits.cpp
+++ 338. Counting-Bits.cpp
@@ -10,11 +10,10 @@ Space complexity: O(n)
 class Solution {
 public:
     vector<int> countBits(int num) {
-        if(num < 1) return vector<int>(1, num);
-        
+
         vector<int> f(num + 1, 0);
-        f[1] = 1;
-        for(int i = 2; i <= num; ++i)
+
+        for(int i = 1; i <= num; ++i)
         {
             f[i] = f[i >> 1] + i % 2;
         }
