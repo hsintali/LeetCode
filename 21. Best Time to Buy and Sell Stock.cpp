b6392ed7fec511941bdc859c26c629f2b368aa82
Hsin-Ta Li <47123885+hsintali@users.noreply.github.com>
2019-10-21 02:31:28 -0700

    Create 21. Best Time to Buy and Sell Stock.cpp

---
+++ 21. Best Time to Buy and Sell Stock.cpp
@@ -0,0 +1,27 @@
+/* https://leetcode.com/problems/best-time-to-buy-and-sell-stock/ */
+
+/* Approach 1: DP
+Intuition:
+
+Time complexity: O(n)
+Space complexity: O(1)
+*/
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices)
+    {
+        if(prices.size() == 0) return 0;
+        
+        int minPrice = prices[0];
+        int result = 0;
+        
+        for(int i = 1; i < prices.size(); ++i)
+        {
+            result = max(result, prices[i] - minPrice);
+            minPrice = min(minPrice, prices[i]);
+        }
+        
+        return result;
+    }
+};
