905f94bafc1e3a847e4d6ca8145b3fa687c088d8
Hsin-Ta Li <47123885+hsintali@users.noreply.github.com>
2019-10-21 03:25:52 -0700

    Update 123. Best Time to Buy and Sell Stock III.cpp

--- 123. Best Time to Buy and Sell Stock III.cpp
+++ 123. Best Time to Buy and Sell Stock III.cpp
@@ -1,7 +1,12 @@
 /* https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iii/ */
 
 /* Approach 1: DP
-Intuition:
+Intuition: 
+5 stages: 1: before 1st buy (w/o stack)
+          2: after 1st buy and before 1st sell (w/ stack)
+          3: after 1st sell and before 2nd buy (w/o stack)
+          4: after 2nd buy and before 2nd sell (w/ stack)
+          5: after 2nd sell (w/o stack)
 
 Time complexity: O(n)
 Space complexity: O(1)
@@ -24,8 +29,10 @@ public:
             {
                 if(j == 1 || j == 3 || j == 5)
                 {
+                    // keep state (w/o stack)
                     f[i%2][j] = f[(i - 1)%2][j];
-
+                    
+                    // sell stack today
                     if(i > 1 && j > 1 && f[(i - 1)%2][j - 1] != INT_MIN)
                     {
                         f[i%2][j] = max(f[(i - 1)%2][j], f[(i - 1)%2][j - 1] + (prices[i - 1] - prices[i - 2]));
@@ -33,8 +40,10 @@ public:
                 }                
                 else if(j == 2 || j == 4)
                 {
+                    // buy stack today
                     f[i%2][j] = f[(i - 1)%2][j - 1];
                     
+                    // keep state (w/ stack)
                     if(i > 1 && f[(i - 1)%2][j] != INT_MIN)
                     {
                         f[i%2][j] = max(f[(i - 1)%2][j] + (prices[i - 1] - prices[i - 2]), f[(i - 1)%2][j - 1]);
