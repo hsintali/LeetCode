ed8241e657a2b98705b63d85123b5848f8d943bd
Hsin-Ta Li <47123885+hsintali@users.noreply.github.com>
2019-11-17 08:07:22 -0800

    Create 141. Linked List Cycle.cpp

---
+++ 141. Linked List Cycle.cpp
@@ -0,0 +1,38 @@
+/* https://leetcode.com/problems/linked-list-cycle/ */
+
+/* Approach 1: fast-slow two pointer
+Intuition:
+Time complexity: O(n)
+Space complexity: O(1)
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(head == NULL) return false;
+        
+        ListNode *slow = head;
+        ListNode *fast = head;
+        
+        while(fast->next != NULL && fast->next->next != NULL)
+        {
+            fast = fast->next->next;
+            slow = slow->next;
+            
+            if(fast == slow)
+            {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+};
