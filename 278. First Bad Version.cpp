a61db836ec404ccf050326189b77d65f145f1387
Hsin-Ta Li <47123885+hsintali@users.noreply.github.com>
2020-01-10 01:39:07 +0800

    Create 278. First Bad Version.cpp

---
+++ 278. First Bad Version.cpp
@@ -0,0 +1,42 @@
+/* https://leetcode.com/problems/first-bad-version/ */
+
+/* Approach 1: binary search
+Intuition:
+Time complexity: O(log n)
+Space complexity: O(1)
+*/
+
+// Forward declaration of isBadVersion API.
+bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n)
+    {
+        int start = 1;
+        int end = n;
+        
+        while(start + 1 < end)
+        {
+            int mid = start + (end - start)/2;
+            
+            if(isBadVersion(mid))
+            {
+                end = mid;
+            }
+            else
+            {
+                start = mid;
+            }
+        }
+        
+        if(isBadVersion(start))
+        {
+            return start;
+        }
+        else
+        {
+            return end;
+        }
+    }
+};
