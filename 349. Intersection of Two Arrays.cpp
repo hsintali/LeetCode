3528ee6410fb9f72656801a399df5dfc69ad062d
Hsin-Ta Li <47123885+hsintali@users.noreply.github.com>
2020-01-10 17:32:26 +0800

    Create 349. Intersection of Two Arrays.cpp

---
+++ 349. Intersection of Two Arrays.cpp
@@ -0,0 +1,67 @@
+/* https://leetcode.com/problems/intersection-of-two-arrays/ */
+
+/* Approach 1: sort + binary search
+Intuition: 先對 nums2 排序，然後遍歷 nums1 各個元素查找 nums2 是否存在，若存在則存到 set 中。
+
+Time complexity: O( nlog(m) + mlog(m) ) = O( (n+m)log(m) ), n=nums1.size(), m=nums2.size()
+Space complexity: O(n)
+*/
+
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2)
+    {
+        if(nums1.size() == 0 || nums2.size() == 0) return vector<int>();
+        
+        vector<int> ans;
+        unordered_set<int> set;
+        
+        sort(nums2.begin(), nums2.end());
+        
+        for(int i = 0; i < nums1.size(); ++i)
+        {
+            if(binarySearch(nums2, nums1[i]))
+            {
+                set.insert(nums1[i]);
+            }
+        }
+        
+        for(auto it = set.begin(); it != set.end(); ++it)
+        {
+            ans.push_back(*it);
+        }
+        
+        return ans;
+    }
+    
+    bool binarySearch(vector<int> &A, int target)
+    {
+        int start = 0;
+        int end = A.size() - 1;
+        
+        while(start + 1 < end)
+        {
+            int mid = start + (end - start)/2;
+            
+            if(A[mid] == target)
+            {
+                return true;
+            }
+            else if(A[mid] > target)
+            {
+                end = mid;
+            }
+            else if(A[mid] < target)
+            {
+                start = mid;
+            }
+        }
+        
+        if(A[start] == target || A[end] == target)
+        {
+            return true;
+        }
+        
+        return false;
+    } 
+};
