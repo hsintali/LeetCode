df2c8577cbe5deebf326e82d31e5795b16f7b939
Hsin-Ta Li <47123885+hsintali@users.noreply.github.com>
2020-01-22 20:08:38 -0800

    Update 155. Min Stack.cpp

--- 155. Min Stack.cpp
+++ 155. Min Stack.cpp
@@ -59,9 +59,60 @@ public:
  * int param_4 = obj->getMin();
  */
  
- /* Approach 1: 1 stack + pair
-
+ /* Approach 2: 1 stack + pair
+Intuition: 用 pair 存放 val 及當下最小值
+           
 Time complexity: O(1)
 Space complexity: O(n)
 */
 
+class MinStack {
+public:
+    /** initialize your data structure here. */
+    MinStack()
+    {
+        soFarMin = INT_MAX;
+    }
+    
+    void push(int x)
+    {
+        soFarMin = min(soFarMin, x);
+        stack_.push({x, soFarMin});
+    }
+    
+    void pop()
+    {
+        stack_.pop();
+        
+        if(!stack_.empty())
+        {
+            soFarMin = stack_.top().second;
+        }
+        else
+        {
+            soFarMin = INT_MAX;
+        }
+    }
+    
+    int top()
+    {
+        return stack_.top().first;
+    }
+    
+    int getMin()
+    {
+        return stack_.top().second;
+    }
+    
+    stack<pair<int, int>> stack_;
+    int soFarMin;
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(x);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
