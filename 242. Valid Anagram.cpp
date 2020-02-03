293933a670d5fc588679e616bc6b6f624c749d9e
Hsin-Ta Li <47123885+hsintali@users.noreply.github.com>
2020-02-02 17:15:33 -0800

    Create 242. Valid Anagram.cpp

---
+++ 242. Valid Anagram.cpp
@@ -0,0 +1,49 @@
+/* https://leetcode.com/problems/valid-anagram/ */
+
+/* Approach 1: Hash
+Intuition:
+
+Time complexity: O(n)
+Space complexity: O(n)
+*/
+
+class Solution {
+public:
+    bool isAnagram(string s, string t)
+    {
+        if(s.size() != t.size()) return false;
+        
+        unordered_map<char,int> hashMap;
+        
+        for(auto c : s)
+        {
+            if(hashMap.find(c) == hashMap.end())
+            {
+                hashMap[c] = 1;
+            }
+            else
+            {
+                ++hashMap[c];
+            }
+        }
+        
+        for(auto c : t)
+        {
+            if(hashMap.find(c) == hashMap.end())
+            {
+                return false;
+            }
+            else
+            {
+                if(hashMap[c] <= 0)
+                {
+                    return false;
+                }
+                
+                --hashMap[c];
+            }
+        }
+            
+        return true;
+    }
+};
