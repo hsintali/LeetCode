e7912969efd5d3369b36fbbbae9dd29b2235f497
Hsin-Ta Li <47123885+hsintali@users.noreply.github.com>
2020-02-18 09:13:30 -0800

    Update 387. First Unique Character in a String.cpp

--- 387. First Unique Character in a String.cpp
+++ 387. First Unique Character in a String.cpp
@@ -1,6 +1,43 @@
 /* https://leetcode.com/problems/first-unique-character-in-a-string/ */
 
-/* Approach 1: hashMap
+/* Approach 1: Brute forece
+Intuition:
+
+Time complexity: O(n^2)
+Space complexity: O(1)
+*/
+
+class Solution {
+public:
+    int firstUniqChar(string s)
+    {
+        if(s.size() == 0) return -1;
+        if(s.size() == 1) return 0;
+        
+        for(int i = 0; i < s.size(); ++i)
+        {
+            bool isRepeat = false;
+            
+            for(int j = 0; j < s.size(); ++j)
+            {
+                if(i != j && s[i] == s[j])
+                {
+                    isRepeat = true;
+                    break;
+                }
+            }
+            
+            if(!isRepeat)
+            {
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+};
+
+/* Approach 2: hashMap
 Intuition:
 
 Time complexity: O(n)
