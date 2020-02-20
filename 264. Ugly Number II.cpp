dbf57a3ac0d374e560581d819d59525ecf792496
Hsin-Ta Li <47123885+hsintali@users.noreply.github.com>
2020-02-19 23:57:04 -0800

    Update 264. Ugly Number II.cpp

--- 264. Ugly Number II.cpp
+++ 264. Ugly Number II.cpp
@@ -45,3 +45,27 @@ Intuition:
 Time complexity: O(n)
 Space complexity: O(n)
 */
+
+class Solution {
+public:
+    int nthUglyNumber(int n)
+    {
+        if(n == 1) return 1;
+        
+        vector<int> uglyNumbers(n, 0);
+        uglyNumbers[0] = 1;
+        
+        int p2 = 0, p3 = 0, p5 = 0;
+        
+        for(int i = 1; i < n; ++i)
+        {
+            uglyNumbers[i] = min(uglyNumbers[p2] * 2, min(uglyNumbers[p3] * 3, uglyNumbers[p5] * 5));
+            
+            if(uglyNumbers[i] == uglyNumbers[p2] * 2) ++p2;
+            if(uglyNumbers[i] == uglyNumbers[p3] * 3) ++p3;
+            if(uglyNumbers[i] == uglyNumbers[p5] * 5) ++p5;
+        }
+        
+        return uglyNumbers.back();
+    }
+};
