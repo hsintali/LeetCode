5ef32fa12798f08f980bad72b93d2f239e83bf3c
Hsin-Ta Li <47123885+hsintali@users.noreply.github.com>
2020-02-21 17:11:11 -0800

    Update 973. K Closest Points to Origin.cpp

--- 973. K Closest Points to Origin.cpp
+++ 973. K Closest Points to Origin.cpp
@@ -2,6 +2,7 @@
 
 /* Approach 1: sort
 Intuition:
+
 Time complexity: O(nlogn)
 Space complexity: O(1)
 */
@@ -20,8 +21,56 @@ public:
     }
 };
 
-/* Approach 2: hashMap + minHeap
+/* Approach 2: hashMap + maxHeap
 Intuition:
-Time complexity: 
+
+Time complexity: O(nlogk)
 Space complexity: O(k)
+*/
+
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int K)
+    {
+        unordered_map<int, int> umap; // key:index, val:distance
+        
+        // O(n)
+        for(int i = 0; i < points.size(); ++i)
+        {
+            umap[i] = points[i][0] * points[i][0] + points[i][1] * points[i][1];
+        }
+        
+        // pair.first:distance, pair.second: index
+        priority_queue<pair<int, int>> maxHeap;
+        
+        // O(nlogk)
+        for(auto &point : umap)
+        {
+            maxHeap.push(make_pair(point.second, point.first));
+            
+            if(maxHeap.size() > K)
+            {
+                maxHeap.pop();
+            }
+        }
+        
+        vector<vector<int>> ans;
+        
+        // O(klogk)
+        for(int i = 0; i < K; ++i)
+        {
+            int index = maxHeap.top().second;
+            maxHeap.pop();
+            ans.push_back(points[index]);
+        }
+        
+        return ans;
+    }
+};
+
+/* Approach 3: Quick Select
+Intuition:
+
+Time complexity: O(n)
+Space complexity: O(1)
 */
