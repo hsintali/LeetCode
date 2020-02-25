46f021aeaf6791764cfffc240468599536c39a45
Hsin-Ta Li <47123885+hsintali@users.noreply.github.com>
2020-02-24 23:45:35 -0800

    Create 207. Course Schedule.cpp

---
+++ 207. Course Schedule.cpp
@@ -0,0 +1,59 @@
+/* https://leetcode.com/problems/course-schedule/ */
+
+/* Approach 1: Topological Sorting
+Intuition:
+
+Time complexity: O(V + E)
+Space complexity: O(V + E)
+*/
+
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites)
+    {
+        vector<int> inDegree(numCourses, 0);
+        
+        vector<vector<int>> neighbors(numCourses, vector<int>());
+        
+        // get in-degree
+        for(auto &prerequisite : prerequisites)
+        {
+            // prerequisite[i, j]: j -> i
+            neighbors[prerequisite[1]].push_back(prerequisite[0]);
+            ++inDegree[prerequisite[0]];
+        }
+        
+        queue<int> que;
+        
+        // put nodes with zero in-degree 
+        for(int i = 0; i < numCourses; ++i)
+        {
+            if(inDegree[i] == 0)
+            {
+                que.push(i);
+            }
+        }
+        
+        int courses = 0;
+        
+        // BFS
+        while(!que.empty())
+        {
+            int cur = que.front();
+            que.pop();
+            ++courses;
+            
+            for(auto &neighbor : neighbors[cur])
+            {
+                --inDegree[neighbor];
+                
+                if(inDegree[neighbor] == 0)
+                {
+                    que.push(neighbor);
+                }
+            }
+        }
+        
+        return courses == numCourses;
+    }
+};
