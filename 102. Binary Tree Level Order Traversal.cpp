06d8509ebad6cd82ea6bef1ad047d0358f14ae35
Hsin-Ta Li <47123885+hsintali@users.noreply.github.com>
2020-03-18 18:18:12 -0700

    Create 102. Binary Tree Level Order Traversal.cpp

---
+++ 102. Binary Tree Level Order Traversal.cpp
@@ -0,0 +1,59 @@
+/* https://leetcode.com/problems/binary-tree-level-order-traversal/ */
+
+/* Approach 1: BFS
+Intuition:
+
+Time complexity: O(V + E) = O(n)
+Space complexity: O(V + E) = O(n)
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root)
+    {
+        vector<vector<int>> ans;
+        
+        if(root == nullptr) return ans;
+        
+        queue<TreeNode*> que;
+        que.push(root);
+        ans.push_back({root->val});
+        
+        while(!que.empty())
+        {
+            vector<int> curLevel;
+            
+            int size = que.size();
+            for(int i = 0; i < size; ++i)
+            {
+                TreeNode *cur = que.front();
+                que.pop();
+                
+                if(cur->left != nullptr)
+                {
+                    que.push(cur->left);
+                    curLevel.push_back(cur->left->val);
+                }
+                
+                if(cur->right != nullptr)
+                {
+                    que.push(cur->right);
+                    curLevel.push_back(cur->right->val);
+                }
+            }
+            
+            if(!curLevel.empty()) ans.push_back(curLevel);
+        }
+        
+        return ans;
+    }
+};
