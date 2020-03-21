30ccaf9451a4dd7a0ac1b854df00d9a034113920
Hsin-Ta Li <47123885+hsintali@users.noreply.github.com>
2020-03-20 22:54:27 -0700

    Update 297. Serialize and Deserialize Binary Tree.cpp

--- 297. Serialize and Deserialize Binary Tree.cpp
+++ 297. Serialize and Deserialize Binary Tree.cpp
@@ -1,7 +1,7 @@
 /* https://leetcode.com/problems/serialize-and-deserialize-binary-tree/ */
 
 /* Approach 1: BFS
-Intuition:
+Intuition: 在"子節點處理"，不 push(null)。
 
 Time complexity: O(V + E) = O(n)
 Space complexity: O(V + E) = O(n)
@@ -116,6 +116,102 @@ public:
             }
             
             if(end == data.end()) break;
+        }
+        
+        return head;
+    }
+};
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec;
+// codec.deserialize(codec.serialize(root));
+
+/* Approach 2: BFS
+Intuition: 在"父節點處理"，需要 push(null)。
+
+Time complexity: O(V + E) = O(n)
+Space complexity: O(V + E) = O(n)
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Codec {
+public:
+
+    // Encodes a tree to a single string.
+    string serialize(TreeNode* root)
+    {
+        if(root == nullptr) return "";
+        
+        string result;
+        
+        queue<TreeNode*> que;
+        que.push(root);
+        
+        while(!que.empty())
+        {
+            TreeNode *node = que.front();
+            que.pop();
+            
+            if(node == nullptr)
+            {
+                result += "#,";
+            }
+            else
+            {
+                result += to_string(node->val) + ",";
+                
+                que.push(node->left);
+                que.push(node->right);
+            }
+        }
+        
+        while(result.back() == '#' || result.back() == ',') result.erase(result.end() - 1);
+        
+        return result;
+    }
+
+    // Decodes your encoded data to tree.
+    TreeNode* deserialize(string data)
+    {
+        if(data.size() == 0) return nullptr;
+        
+        TreeNode *head = nullptr;
+        
+        queue<TreeNode**> que;
+        que.push(&head);
+        
+        string::iterator start = data.begin();
+        
+        while(start != data.end())
+        {
+            TreeNode **cur = que.front();
+            que.pop();
+            
+            if(*start != '#')
+            {
+                string::iterator end = find(start, data.end(), ',');
+                int val = stoi(string(start, end));
+                *cur = new TreeNode(val);
+                
+                que.push( &((*cur)->left) );
+                que.push( &((*cur)->right) );
+                
+                if(end == data.end()) break;
+                
+                start = next(end);
+            }
+            else
+            {
+                start += 2;
+            }
         }
         
         return head;
