f68f5cfb14798c2a1d908bea2785eb9295537fbd
Hsin-Ta Li <47123885+hsintali@users.noreply.github.com>
2020-03-30 17:08:32 -0700

    Create 538. Convert BST to Greater Tree.cpp

---
+++ 538. Convert BST to Greater Tree.cpp
@@ -0,0 +1,40 @@
+/* https://leetcode.com/problems/convert-bst-to-greater-tree/ */
+
+/* Approach 1: DFS
+Intuition:
+
+Time complexity: O(V) = O(n)
+Space complexity: O(H)
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* convertBST(TreeNode* root)
+    {
+        int sum = 0;
+        invertInorderTraversal(root, sum);
+        
+        return root;
+    }
+    
+    void invertInorderTraversal(TreeNode *root, int &sum)
+    {
+        if(root == nullptr) return;
+        
+        invertInorderTraversal(root->right, sum);
+        
+        sum += root->val;
+        root->val = sum;
+        
+        invertInorderTraversal(root->left, sum);
+    }
+};
