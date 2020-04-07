747797416fc02800d850ee452c66decddbf85f0e
Hsin-Ta Li <47123885+hsintali@users.noreply.github.com>
2020-04-07 13:49:56 -0700

    Update 272. Closest Binary Search Tree Value II.cpp

--- 272. Closest Binary Search Tree Value II.cpp
+++ 272. Closest Binary Search Tree Value II.cpp
@@ -54,7 +54,92 @@ public:
 /* Approach 2: BST Iterator
 Intuition:
 
-Time complexity: O(H + k) = O(nlogn + k)
+Time complexity: O(H + k) = O(logn + k)
 Space complexity: O(H)
 */
 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> closestKValues(TreeNode* root, double target, int k)
+    {
+        stack<TreeNode*> succStack;
+        stack<TreeNode*> predStack;
+        vector<int> ans;
+        
+        initStack(root, target, succStack, predStack);
+        
+        while(k-- > 0)
+        {
+            if(succStack.empty() || !predStack.empty() && (target - predStack.top()->val) < (succStack.top()->val - target))
+            {
+                ans.push_back(predStack.top()->val);
+                getPredecessor(predStack);
+            }
+            else
+            {
+                ans.push_back(succStack.top()->val);
+                getSuccessor(succStack);
+            }
+        }
+            
+        return ans;
+    }
+    
+    void initStack(TreeNode *node, double target, stack<TreeNode*> &succStack, stack<TreeNode*> &predStack)
+    {
+        while(node != nullptr)
+        {
+            if(node->val <= target)
+            {
+                predStack.push(node);
+                node = node->right;
+            }
+            else
+            {
+                succStack.push(node);
+                node = node->left;
+            }
+        }
+    }
+    
+    void getSuccessor(stack<TreeNode*> &succStack)
+    {
+        TreeNode *node = succStack.top();
+        succStack.pop();
+        
+        if(node->right != nullptr)
+        {
+            node = node->right;
+            while(node != nullptr)
+            {
+                succStack.push(node);
+                node = node->left;
+            }
+        }
+    }
+    
+    void getPredecessor(stack<TreeNode*> &predStack)
+    {
+        TreeNode *node = predStack.top();
+        predStack.pop();
+        
+        if(node->left != nullptr)
+        {
+            node = node->left;
+            while(node != nullptr)
+            {
+                predStack.push(node);
+                node = node->right;
+            }
+        }
+    } 
+};
