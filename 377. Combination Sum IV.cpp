3bffbfd17d8d8acd14d7856ad162e639e21b9d39
Hsin-Ta Li <47123885+hsintali@users.noreply.github.com>
2020-04-10 11:39:15 -0700

    Create 377. Combination Sum IV.cpp

---
+++ 377. Combination Sum IV.cpp
@@ -0,0 +1,63 @@
+/* https://leetcode.com/problems/combination-sum-iv/ */
+
+/* Approach 1: DFS (TLE)
+Intuition:
+
+Time complexity:O()
+Space complexity: O()
+*/
+
+class Solution {
+public:
+    int combinationSum4(vector<int>& nums, int target)
+    {
+        int ans = 0;
+        
+        dfs(nums, target, 0, ans);
+        
+        return ans;
+    }
+    
+    void dfs(vector<int> &nums, int target, int sum, int &ans)
+    {
+        if(sum == target) ++ans;
+        if(sum >= target) return;
+        
+        for(int i = 0; i < nums.size(); ++i)
+        {
+            dfs(nums, target, sum + nums[i], ans);
+        }
+    }
+};
+
+/* Approach 2: DP
+Intuition:
+
+Time complexity:O()
+Space complexity: O()
+*/
+
+class Solution {
+public:
+    int combinationSum4(vector<int>& nums, int target)
+    {
+        vector<unsigned int> dp(target + 1, 0);
+        
+        for(auto &n : nums)
+        {
+            if(n > target) continue;
+            
+            dp[n] = 1;
+        }
+        
+        for(int i = 1; i <= target; ++i)
+        {
+            for(auto &n : nums)
+            {
+                if(i - n >= 0) dp[i] += dp[i - n];
+            }
+        }
+                        
+        return dp[target];
+    }
+};
