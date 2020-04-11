4181170aa8e10b9ceb74a8fa182b82c1e9425549
Hsin-Ta Li <47123885+hsintali@users.noreply.github.com>
2020-04-11 11:04:34 -0700

    Create 543. Diameter of Binary Tree.cpp

---
+++ 543. Diameter of Binary Tree.cpp
@@ -0,0 +1,53 @@
+/* https://leetcode.com/problems/diameter-of-binary-tree/ */
+
+/* Approach 1: DFS
+Intuition:
+
+Time complexity: O(V) = O(n)
+Space complexity: O(H)
+*/
+
+struct resultType
+{
+    int left;
+    int right;
+    int maxPath;
+    resultType(int left, int right, int maxPath)
+    {
+        this->left = left;
+        this->right = right;
+        this->maxPath = maxPath;
+    }
+};
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int diameterOfBinaryTree(TreeNode* root)
+    {
+        return dfs(root).maxPath;
+    }
+    
+    resultType dfs(TreeNode *root)
+    {
+        if(root == nullptr) return resultType(-1, -1, 0);
+        
+        resultType leftResult = dfs(root->left);
+        resultType rightResult = dfs(root->right);
+        
+        int left = max(leftResult.left, leftResult.right) + 1;
+        int right = max(rightResult.left, rightResult.right) + 1;
+        int maxPath = left + right;
+        maxPath = max(leftResult.maxPath, max(maxPath, rightResult.maxPath));
+        
+        return resultType(left, right, maxPath);
+    }
+};
