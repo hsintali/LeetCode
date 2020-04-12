76209074dc0489d66244b15303c11057d3560eb0
Hsin-Ta Li <47123885+hsintali@users.noreply.github.com>
2020-04-11 21:33:41 -0700

    Update 543. Diameter of Binary Tree.cpp

--- 543. Diameter of Binary Tree.cpp
+++ 543. Diameter of Binary Tree.cpp
@@ -51,3 +51,52 @@ public:
         return resultType(left, right, maxPath);
     }
 };
+
+/* Approach 1-2: DFS
+Intuition:
+
+Time complexity: O(V) = O(n)
+Space complexity: O(H)
+*/
+
+struct resultType
+{
+    int maxDepth;
+    int maxPath;
+    resultType(int maxDepth, int maxPath)
+    {
+        this->maxDepth = maxDepth;
+        this->maxPath = maxPath;
+    }
+};
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int diameterOfBinaryTree(TreeNode* root)
+    {
+        return dfs(root).maxPath;
+    }
+    
+    resultType dfs(TreeNode *root)
+    {
+        if(root == nullptr) return resultType(-1, 0);
+        
+        resultType leftResult = dfs(root->left);
+        resultType rightResult = dfs(root->right);
+        
+        int maxDepth = max(leftResult.maxDepth, rightResult.maxDepth) + 1;
+        int maxPath = leftResult.maxDepth + 1 + rightResult.maxDepth + 1;
+        maxPath = max(leftResult.maxPath, max(maxPath, rightResult.maxPath));
+        
+        return resultType(maxDepth, maxPath);
+    }
+};
