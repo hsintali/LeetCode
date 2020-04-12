9c105dc83121a69af573cae5a8a79400b508f05d
Hsin-Ta Li <47123885+hsintali@users.noreply.github.com>
2020-04-11 21:42:06 -0700

    Create 1409. Queries on a Permutation With Key.cpp

---
+++ 1409. Queries on a Permutation With Key.cpp
@@ -0,0 +1,38 @@
+/* https://leetcode.com/problems/queries-on-a-permutation-with-key/ */
+
+/* Approach 1: Bruteforce
+Intuition:
+
+Time complexity:O(qm)
+Space complexity: O(1)
+*/
+
+class Solution {
+public:
+    vector<int> processQueries(vector<int>& queries, int m)
+    {
+        vector<int> ans;
+        
+        vector<int> p(m, 0);
+        for(int i = 0; i < m; ++i)
+        {
+            p[i] = i + 1;
+        }
+        
+        for(auto &q : queries)
+        {
+            for(int i = 0; i < m; ++i)
+            {
+                if(p[i] == q)
+                {
+                    p.erase(p.begin() + i);
+                    p.insert(p.begin(), q);
+                    ans.push_back(i);
+					break;
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
