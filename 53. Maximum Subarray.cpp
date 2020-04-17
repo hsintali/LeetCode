7b25c75295ee0a539599a8b2b31453083f999c9f
Hsin-Ta Li <47123885+hsintali@users.noreply.github.com>
2020-04-16 21:20:19 -0700

    Update 53. Maximum Subarray.cpp

--- 53. Maximum Subarray.cpp
+++ 53. Maximum Subarray.cpp
@@ -22,3 +22,31 @@ public:
         return maxSum;
     }
 };
+
+/* Approach 1: PrefixSum
+Intuition:
+
+Time complexity: O(n)
+Space complexity: O(1)
+*/
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums)
+    {
+        int maxSum = INT_MIN;
+        int prefixSum_cur = 0;
+        int prefixSum_min = 0;
+        
+        for(auto &num : nums)
+        {
+            prefixSum_cur += num;
+            
+            maxSum = max(maxSum, prefixSum_cur - prefixSum_min);
+
+            prefixSum_min = min(prefixSum_min, prefixSum_cur);
+        }
+        
+        return maxSum;
+    }
+};
