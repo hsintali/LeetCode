7c101652d16f2964da5d2c04a494f3086f6a5cdf
Hsin-Ta Li <47123885+hsintali@users.noreply.github.com>
2020-04-17 23:41:01 -0700

    Update 4. Median of Two Sorted Arrays.cpp

--- 4. Median of Two Sorted Arrays.cpp
+++ 4. Median of Two Sorted Arrays.cpp
@@ -130,3 +130,94 @@ public:
         return arr[right + 1];
     }
 };
+
+/* Approach 4: Binary Search
+Intuition:
+Time complexity: O( (log(n) + log(m)) ∗ log(max−min))
+Space complexity: O(1)
+*/
+
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2)
+    {
+        int len = nums1.size() + nums2.size();
+        
+        if(len % 2 == 0)
+        {
+            return (findKth(nums1, nums2, len / 2) + findKth(nums1, nums2, len / 2 + 1)) / 2;
+        }
+        
+        return findKth(nums1, nums2, len / 2 + 1);
+    }
+    
+    double findKth(vector<int> &nums1, vector<int> &nums2, int k)
+    {
+        if(nums1.size() == 0) return nums2[k - 1];
+        if(nums2.size() == 0) return nums1[k - 1];
+        
+        int n = nums1.size();
+        int m = nums2.size();
+        int start = min(nums1[0], nums2[0]);
+        int end = max(nums1[n - 1], nums2[m - 1]);
+        int count = 0;
+        
+        while(start + 1 < end)
+        {
+            int mid = start + ((end - start) >> 1);
+            
+            count = getLessOrEqualCount(nums1, mid) + getLessOrEqualCount(nums2, mid);
+            
+            if(count >= k)
+            {
+                end = mid;
+            }
+            else if(count < k)
+            {
+                start = mid;
+            }
+        }
+        
+        if(getLessOrEqualCount(nums1, start) + getLessOrEqualCount(nums2, start) >= k)
+        {
+            return start;
+        }
+        
+        return end;
+    }
+    
+    int getLessOrEqualCount(vector<int> &arr, int target)
+    {
+        int start = 0;
+        int end = arr.size() - 1;
+        
+        while(start + 1 < end)
+        {
+            int mid = start + ((end - start) >> 1);
+            
+            if(arr[mid] == target)
+            {
+                return mid + 1;
+            }
+            else if(arr[mid] <= target)
+            {
+                start = mid;
+            }
+            else if(arr[mid] > target)
+            {
+                end = mid;
+            }
+        }
+        
+        if(arr[start] > target) return start;
+        if(arr[end] > target) return end;
+        return arr.size();
+    }
+};
+
+/* Approach 5: Binary Search
+Intuition:
+Time complexity: O(log(n + m))
+Space complexity: O(1)
+*/
+
