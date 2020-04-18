db3bce4ec6f222d19189a49e1537bc0123674ccb
Hsin-Ta Li <47123885+hsintali@users.noreply.github.com>
2020-04-17 22:49:46 -0700

    Update 4. Median of Two Sorted Arrays.cpp

--- 4. Median of Two Sorted Arrays.cpp
+++ 4. Median of Two Sorted Arrays.cpp
@@ -1,6 +1,6 @@
 /* https://leetcode.com/problems/median-of-two-sorted-arrays/ */
 
-/* Approach 2:
+/* Approach 1:
 Intuition: 直接排序後選出中位數。
 
 Time complexity:O( (n + m) * log(n + m) )
@@ -65,5 +65,68 @@ public:
         }
         
         return nums1[nums1.size() / 2];
+    }
+};
+
+/* Approach 3: quick selection
+Intuition: 
+
+Time complexity:O(n + m)
+Space complexity: O(abs(m-n))
+*/
+
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2)
+    {
+        nums1.insert(nums1.end(), nums2.begin(), nums2.end());
+        
+        int k = nums1.size() / 2 + 1;
+        double ans = quickSelect(nums1, 0, nums1.size() - 1, k);
+        
+        if(nums1.size() % 2 == 0)
+        {
+            ans += quickSelect(nums1, 0, nums1.size() - 1, k - 1);
+            ans /= 2;
+        }
+            
+        return ans;        
+    }
+    
+    int quickSelect(vector<int> &arr, int start, int end, int k)
+    {
+        if(start == end) return arr[start];
+        
+        int left = start;
+        int right = end;
+        int pivot = arr[start + ((end - start) >> 1)];
+        
+        while(left <= right)
+        {
+            while(left <= right && arr[left] > pivot) ++left;
+            while(left <= right && arr[right] < pivot) --right;
+            
+            if(left <= right)
+            {
+                int temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+                ++left;
+                --right;
+            }
+        }
+        
+        // [start, ..., right, , left, ..., end]
+        if(start + k - 1 <= right)
+        {
+            return quickSelect(arr, start, right, k);
+        }
+        
+        if(start + k - 1 >= left)
+        {
+            return quickSelect(arr, left, end, k - (left - start));
+        }
+        
+        return arr[right + 1];
     }
 };
