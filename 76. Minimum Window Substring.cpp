7e729a4fdf9ce692f96beda8e89de2400caf2150
Hsin-Ta Li <47123885+hsintali@users.noreply.github.com>
2020-04-21 21:52:04 -0700

    Update 76. Minimum Window Substring.cpp

--- 76. Minimum Window Substring.cpp
+++ 76. Minimum Window Substring.cpp
@@ -60,3 +60,61 @@ public:
         return s.substr(ansL, ansR - ansL);
     }
 };
+
+/* Approach 1-2: Hash + Sliding Window
+Intuition:
+
+Time complexity:  O(S + T)
+Space complexity: O(S + T)
+*/
+
+class Solution {
+public:
+    string minWindow(string s, string t)
+    {
+        if(s.size() < t.size()) return "";
+        
+        int S = 0, T = 0;
+        
+        int countS[256];
+        for(int i = 0; i < 255; ++i) countS[i] = 0;
+        
+        int countT[256];
+        for(int i = 0; i < 255; ++i) countT[i] = 0;
+        for(auto & c : t)
+        {
+            ++countT[c];
+            if(countT[c] == 1) ++T;
+        }
+        
+        int ansL = -1, ansR = -1;
+        int left = 0, right = 0;
+        for(left = 0; left < s.size(); ++left)
+        {
+            
+            while(S < T && right < s.size())
+            {
+                int c = s[right];
+                ++countS[c];
+                if(countS[c] == countT[c]) ++S;
+                ++right;
+            }
+            
+            if(S == T && (ansL == -1 ||  right - left < ansR - ansL))
+            {
+                ansL = left;
+                ansR = right;
+            }
+            
+            --countS[s[left]];
+            if(countS[s[left]] == countT[s[left]] - 1)
+            {
+                --S;
+            }
+        }
+        
+        if(ansL == -1) return "";
+        
+        return s.substr(ansL, ansR - ansL);
+    }
+};
