c324f44778311342caca6d356038a42623e0928a
Hsin-Ta Li <47123885+hsintali@users.noreply.github.com>
2020-04-27 19:28:34 -0700

    Update 221. Maximal Square.cpp

--- 221. Maximal Square.cpp
+++ 221. Maximal Square.cpp
@@ -39,3 +39,53 @@ public:
         return maxLen * maxLen;
     }
 };
+
+/* Approach 1-2: DP
+Intuition:
+
+Time complexity:  O(mn)
+Space complexity: O(mn)
+*/
+
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix)
+    {
+        int n = matrix.size();
+        if(n == 0) return 0;
+        
+        int m = matrix[0].size();
+        if(m == 0) return 0;
+        
+        int maxLen = 0;
+        int dp[n][m];
+        
+        for(int i = 0; i < n; ++i)
+        {
+            for(int j = 0; j < m; ++j)
+            {
+                dp[i][j] = 0;
+                
+                if(i == 0 || j == 0)
+                {
+                    if(matrix[i][j] == '1')
+                    {
+                        dp[i][j] = 1;
+                    }
+                    
+                    maxLen = max(maxLen, dp[i][j]);
+                    
+                    continue;
+                }
+                
+                if(matrix[i][j] != '1') continue;
+                
+                dp[i][j] = min(dp[i - 1][j - 1], min(dp[i - 1][j], dp[i][j - 1])) + 1;
+                
+                maxLen = max(maxLen, dp[i][j]);
+            }
+        }
+        
+        return maxLen * maxLen;
+    }
+};
