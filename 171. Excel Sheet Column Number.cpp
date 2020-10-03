1ca92305e2038d10817162915bcc57ad8b996063
Hsin-Ta Li <47123885+hsintali@users.noreply.github.com>
2020-10-04 06:30:18 +0800

    Update 171. Excel Sheet Column Number.cpp

--- 171. Excel Sheet Column Number.cpp
+++ 171. Excel Sheet Column Number.cpp
@@ -23,3 +23,25 @@ public:
         return ans;
     }
 };
+
+/* Approach 2:
+Intuition:
+
+Time complexity:  O(n)
+Space complexity: O(1)
+*/
+
+class Solution {
+public:
+    int titleToNumber(string s)
+    {
+        long long ans{};
+        
+        for(int i = 0; i < s.size(); ++i)
+        {
+            ans = ans * 26 + (s[i] - 'A' + 1);
+        }
+        
+        return ans;
+    }
+};
