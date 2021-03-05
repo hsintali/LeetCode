1d524f86adfdc40acee1541644ae947a2ab26dd1
Hsin-Ta Li <47123885+hsintali@users.noreply.github.com>
2021-03-05 22:59:06 +0800

    Update 637. Average of Levels in Binary Tree.cpp

--- 637. Average of Levels in Binary Tree.cpp
+++ 637. Average of Levels in Binary Tree.cpp
@@ -51,3 +51,56 @@ public:
         return ans;
     }
 };
+
+/* Approach 2: DFS
+Intuition:
+
+Time complexity:  O(n), where n is the number of nodes in the given binary tree
+Space complexity: O(h), where h is the height of the tree
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<double> averageOfLevels(TreeNode* root)
+    {
+        vector<double> ans;
+        vector<int> count;
+        dfs(root, 0, ans, count);
+        for(int i = 0; i < ans.size(); ++i)
+        {
+            ans[i] /= count[i];
+        }
+        
+        return ans;        
+    }
+    
+    void dfs(TreeNode *node, int i, vector<double> &ans, vector<int> &count)
+    {
+        if(node == nullptr) return;
+        
+        if(i < ans.size())
+        {
+            ans[i] += node->val;
+            ++count[i];
+        }
+        else
+        {
+            ans.push_back(node->val);
+            count.push_back(1);
+        }
+        
+        dfs(node->left, i + 1, ans, count);
+        dfs(node->right, i + 1, ans, count);
+    }
+};
