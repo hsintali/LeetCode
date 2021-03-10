3b9751bbbf2baafd36a01aaaf279e4d56dc9ba23
Hsin-Ta Li <47123885+hsintali@users.noreply.github.com>
2021-03-10 10:28:48 +0800

    Update 623. Add One Row to Tree.cpp

--- 623. Add One Row to Tree.cpp
+++ 623. Add One Row to Tree.cpp
@@ -2,6 +2,7 @@
 
 /* Approach 1: BFS
 Intuition:
+
 Time complexity: O(n), where n is the number of nodes
 Space complexity: O(log(n))
 */
@@ -53,6 +54,54 @@ public:
             }
             
             ++level;
+        }
+        
+        return root;
+    }
+};
+
+/* Approach 2: DFS (recursive)
+Intuition: 
+
+Time complexity: O(n), where n is the number of nodes
+Space complexity: O(log(n))
+*/
+
+
+/* Approach 3: DFS (compact code without help function)
+Intuition: In addition to use 1 to indicate attach to left node as required, 
+              we can also use 0 to indicate attach to right node.
+
+Time complexity: O(n), where n is the number of nodes
+Space complexity: O(log(n))
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* addOneRow(TreeNode* root, int v, int d)
+    {
+        if(d < 2)
+        {
+            TreeNode *newNode = new TreeNode(v);
+            (d == 1 ? newNode->left : newNode->right) = root;
+            return newNode;
+        }
+        
+        if(root)
+        {
+            root->left = addOneRow(root->left, v, d - 1);
+            root->right = addOneRow(root->right, v, d - 1 - (d == 2));
         }
         
         return root;
