2550a5633e2da4519a8c982135f171e8ba5b9e53
Hsin-Ta Li <47123885+hsintali@users.noreply.github.com>
2021-03-10 10:52:23 +0800

    Update 623. Add One Row to Tree.cpp

--- 623. Add One Row to Tree.cpp
+++ 623. Add One Row to Tree.cpp
@@ -4,7 +4,7 @@
 Intuition:
 
 Time complexity: O(n), where n is the number of nodes
-Space complexity: O(log(n))
+Space complexity: O(log(n)), the number of maximum number of nodes at any level in the given tree
 */
 
 /**
@@ -64,16 +64,57 @@ public:
 Intuition: 
 
 Time complexity: O(n), where n is the number of nodes
-Space complexity: O(log(n))
+Space complexity: O(d), depth
 */
 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* addOneRow(TreeNode* root, int v, int d)
+    {
+        if(d == 1)
+        {
+            return new TreeNode(v, root, nullptr);
+        }
+        
+        dfs(root, v, d, 1);
+        
+        return root;
+    }
+    
+    void dfs(TreeNode *root, int v, int d, int level)
+    {
+        if(root == nullptr || level >= d) return;
+        
+        if(level < d - 1)
+        {
+            dfs(root->left, v, d, level + 1);
+            dfs(root->right, v, d, level + 1);
+        }
+        else
+        {
+            root->left = new TreeNode(v, root->left, nullptr);
+            root->right = new TreeNode(v, nullptr, root->right);
+        }
+    }
+};
 
 /* Approach 3: DFS (compact code without help function)
 Intuition: In addition to use 1 to indicate attach to left node as required, 
               we can also use 0 to indicate attach to right node.
 
 Time complexity: O(n), where n is the number of nodes
-Space complexity: O(log(n))
+Space complexity: O(d), depth
 */
 
 /**
@@ -107,3 +148,11 @@ public:
         return root;
     }
 };
+
+/* Approach 4: DFS (queue)
+Intuition: 
+
+Time complexity: O(n), where n is the number of nodes
+Space complexity: O(d), depth
+*/
+
